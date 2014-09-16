ab58979305ee8f85541b7f9d53a9f55b815581b1
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-09-16 16:24:37 -0700

    Whitespace

--- formats/format.php
+++ formats/format.php
@@ -1,3 +1,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( spine_get_option( 'articletitle_show' ) == 'true' ) : ?>
+	<header class="article-header">
+		<h1 class="article-title"><?php the_title(); ?></h1>
+	</header>
+	<?php endif; ?>
 	<?php the_content(); ?>
 </article>
