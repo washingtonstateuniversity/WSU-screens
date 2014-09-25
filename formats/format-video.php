783c8388b4b904c66e9cfff706248d396b68a874
jeremyfelt <jeremy.felt@gmail.com>
2014-09-25 11:48:43 -0500

    Escape data in video format

--- formats/format-video.php
+++ formats/format-video.php
@@ -11,13 +11,13 @@ if (has_tag('home')) { $loop = " loop"; $ytloop = "&loop=1"; } else { $loop = ""
 	
 <?php if ( $videolink != '' ) : ?>
 				
-	<video class="video-js vjs-default-skin" controls autoplay<?php echo $loop; ?> preload="auto" width="1920" height="1080" poster="" data-setup='{"example_option":true}'>
-		<source src="<?php echo get_post_meta($post->ID, 'video', true); ?>" type='video/mp4' />
+	<video class="video-js vjs-default-skin" controls autoplay<?php echo esc_attr( $loop ); ?> preload="auto" width="1920" height="1080" poster="" data-setup='{"example_option":true}'>
+		<source src="<?php echo esc_url( get_post_meta($post->ID, 'video', true) ); ?>" type='video/mp4' />
 	</video>
 	
 	<script>
 	if ($('video').length) {
-		$('video').get(0).volume = <?php echo $audiolevel; ?>;
+		$('video').get(0).volume = <?php echo esc_js( $audiolevel ); ?>;
 		}
 	</script>
 	
@@ -39,14 +39,14 @@ if (has_tag('home')) { $loop = " loop"; $ytloop = "&loop=1"; } else { $loop = ""
     player = new YT.Player('ytplayer', {
       height: '1080',
       width: '1920',
-      videoId: '<?php echo get_post_meta($post->ID, 'youtube', true); ?>',
+      videoId: '<?php echo esc_url( get_post_meta($post->ID, 'youtube', true) ); ?>',
       controls: '0',
       autoplay: '1'
     });
   }
 </script>-->
 	
-<iframe width="1920" height="1080" src="//www.youtube.com/embed/<?php echo get_post_meta($post->ID, 'youtube', true); ?>?version=3&enablejsapi=1&rel=0&showinfo=0&autoplay=1&controls=0&html5=1<?php echo $ytloop; ?>&volume=100" frameborder="0"  scrolling="yes" allowfullscreen></iframe>
+<iframe width="1920" height="1080" src="//www.youtube.com/embed/<?php echo urlencode( get_post_meta($post->ID, 'youtube', true) ); ?>?version=3&enablejsapi=1&rel=0&showinfo=0&autoplay=1&controls=0&html5=1<?php echo urlencode( $ytloop ); ?>&volume=100" frameborder="0"  scrolling="yes" allowfullscreen></iframe>
 	
 <?php endif; ?>
 		
